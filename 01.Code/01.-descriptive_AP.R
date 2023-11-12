c267444941677ea45cde72e02f6d9170749a57a4
alldominguez <aidominguezmontoya@gmail.com>
2023-11-12 20:43:36 +0100

    update code

---
+++ 01.Code/01.-descriptive_AP.R
@@ -0,0 +1,121 @@
+# Install packages
+pacman::p_load(tidyverse)
+
+##############################
+### --- Load the data --- ###
+############################
+
+#################################################
+### --- Air pollutants measurements data --- #####################################################################
+################################################
+no2_measures <- read.csv("01.Data/BiSC_comparison_data/measures/no2_final_model_data.csv")
+pm25_measures <- read.csv("01.Data/BiSC_comparison_data/measures/pm25_final_model_data.csv")
+bc_measures <- read.csv("01.Data/BiSC_comparison_data/measures/bc_final_model_data.csv")
+pm25_constituents_measures <- read.csv("01.Data/BiSC_comparison_data/measures/pm25_constituents_model_data.csv")
+
+
+# Have a quick look to the data 
+dplyr::glimpse(no2_measures)
+dplyr::glimpse(pm25_measures)
+dplyr::glimpse(bc_measures)
+dplyr::glimpse(pm25_constituents_measures)
+
+### --- selection of specific variables --- ###
+no2_measures <- no2_measures %>%
+  dplyr::select(gid, id, year, week, NO2.DirectMeasurements, lon, lat) 
+
+pm25_measures <- pm25_measures %>% 
+  dplyr::select(gid, site_id, sid, c_biscape, lon, lat)
+
+bc_measures <- bc_measures %>% 
+  dplyr::select(gid, site_id, sid, c_biscape, lon, lat)
+
+pm25_constituents_measures <- pm25_constituents_measures %>% 
+  dplyr::select(gid, site_id, sid, 
+                biscape_Fe2O3,biscape_Cu, biscape_Zn, lon, lat)
+
+### --- rename some variables --- ###
+no2_measures <- no2_measures %>% 
+  dplyr::rename(no2_measures = NO2.DirectMeasurements)
+
+pm25_measures <- pm25_measures %>% 
+  dplyr::rename(pm25_measures = c_biscape) 
+
+bc_measures <- bc_measures %>% 
+  dplyr::rename(bc_measures = c_biscape)
+
+pm25_constituents_measures <- pm25_constituents_measures %>% 
+  dplyr::rename(fe2o3_measures = biscape_Fe2O3, 
+                cu_measures = biscape_Cu, 
+                zn_measures = biscape_Zn)
+
+#######################################
+### --- Model predictions data --- #################################################################################################
+######################################
+
+##############################
+### --- Hybrid models --- ###
+#############################
+no2_hybrid <- read.csv("01.Data/BiSC_comparison_data/predictions/HM/weekly_estimates_no2_hybrid_model.csv")
+pm25_hybrid <- read.csv("01.Data/BiSC_comparison_data/predictions/HM/weekly_estimates_pm25_hybrid_model.csv")
+bc_hybrid <- read.csv("01.Data/BiSC_comparison_data/predictions/HM/weekly_estimates_bc_hybrid_model.csv")
+pm25_constituents_hybrid <- read.csv("01.Data/BiSC_comparison_data/predictions/HM/weekly_estimates_pm25constituents_hybrid_model.csv")
+
+dplyr::glimpse(no2_hybrid)
+dplyr::glimpse(pm25_hybrid)
+dplyr::glimpse(bc_hybrid)
+dplyr::glimpse(pm25_constituents_hybrid)
+
+# --- rename variables --- # 
+
+no2_hybrid <- no2_hybrid %>% 
+              dplyr::rename(no2_hm = no2.total_hybridmodel)
+
+pm25_hybrid <- pm25_hybrid %>% 
+               dplyr::rename()
+                
+
+
+
+##################################
+### --- Dispersion models --- ###
+#################################
+
+
+
+
+
+
+
+###########################
+### --- LUR models --- ###
+#########################
+
+
+
+
+
+
+
+
+
+#######################################
+### --- Model predictions data --- ###
+######################################
+
+
+
+
+
+
+
+
+
+
+#####################################################################################
+
+# have a quick look to the data 
+no2_measures %>% dplyr::glimpse()
+pm25_measures %>% dplyr::glimpse()
+bc_measures %>% dplyr::glimpse()
+pm25_constituents_measures %>% dplyr::glimpse()
