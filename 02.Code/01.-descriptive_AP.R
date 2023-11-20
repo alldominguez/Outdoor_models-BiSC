d04e313a5cc86c3a3715df16f3a8d24fd17a0c68
alldominguez <aidominguezmontoya@gmail.com>
2023-11-20 18:39:26 +0100

    plotting maps

--- 02.Code/01.-descriptive_AP.R
+++ 02.Code/01.-descriptive_AP.R
@@ -1,5 +1,6 @@
 # Install packages
-pacman::p_load(tidyverse, purrr, skimr, caret, tictoc, lubridate,  mice, ggmice, plyr, data.table)
+pacman::p_load(tidyverse, purrr, skimr, caret, tictoc, lubridate, 
+               mice, ggmice, plyr, data.table, mapview, ggmap, omsdata)
 
 ##############################
 ### --- Load the data --- ###
@@ -8,8 +9,24 @@ pacman::p_load(tidyverse, purrr, skimr, caret, tictoc, lubridate,  mice, ggmice,
 ###############################
 ### --- Home addresses --- ###
 #############################
-bisc_home_coords <- read.csv() # wee need assign to dm and lur data the lon/lat to then plot the predictions (entire pregnancy)
+gid_data <- read.csv("01.Data/hybrid_data_final_040823.csv") # wee need assign to dm and lur data the lon/lat to then plot the predictions (entire pregnancy)
+dplyr::glimpse(gid_data)
 
+gid_data <- gid_data %>% dplyr::select(gid, subject_id, weeks, lon, lat)
+
+gid_data$weeks <- dplyr::recode_factor(gid_data$weeks, 
+                                       w1 = 'w01', w2 = 'w02', w3 = 'w03', w4 = 'w04', w5 = 'w05',
+                                       w6 = 'w06', w7 = 'w07', w8 = 'w08', w9 = 'w09', w10 = 'w10',
+                                       w11 = 'w11', w12 = 'w12', w13 = 'w13', w14 = 'w14', w15 = 'w15',
+                                       w16 = 'w16', w17 = 'w17', w18 = 'w18', w19 = 'w19', w20 = 'w20',
+                                       w21 = 'w21', w22 = 'w22', w23 = 'w23', w24 = 'w24', w25 = 'w25',
+                                       w26 = 'w26', w27 = 'w27', w28 = 'w28', w29 = 'w29', w30 = 'w30',
+                                       w31 = 'w31', w32 = 'w32', w33 = 'w33', w34 = 'w34', w35 = 'w35',
+                                       w36 = 'w36', w37 = 'w37', w38 = 'w38', w39 = 'w39', w40 = 'w40',
+                                       w41 = 'w41', w42 = 'w42', w43 = 'w43', w44 = 'w44')
+
+gid_data <- gid_data %>% dplyr::arrange(subject_id, weeks)
+dplyr::glimpse(gid_data)
 
 #############################################
 ### --- Air pollutants measurements data --- #####################################################################
@@ -258,16 +275,52 @@ lur_estimates <- no2_lur %>%
 dplyr::glimpse(lur_estimates)
 lur_estimates %>% ungroup() %>% skimr::skim() # we need to ungroup first 
 
+# add the longitude and latitude 
+lur_estimates <- lur_estimates %>% 
+                 dplyr::left_join(gid_data, 
+                                  by = c("subject_id", "weeks"))
+
+dplyr::glimpse(lur_estimates)
+
+############################
+### --- Export data --- ###
+##########################
+rio::export(lur_estimates, "01.Data/lur_estimates.csv")
+
+##############################################################################################
+
+# Note 1: to plot the estimates we going to group by gid
+# Note 2: to summarize the long-term exposure we group by 
+
+####################################
+### --- Plotting estimates --- ###
+##################################
+
+### -- LUR estimates --- ### 
+lur_estimates_plot <- lur_estimates %>% 
+                      dplyr::group_by(gid) # we have 1.235 home address
+
+dplyr::glimpse(lur_estimates_plot)
+
+### --- Load shapefile  Barcelona metropolitan --- ###b
+bcn_shp <- sf::read_sf("01.Data/BCN_GIS_data/bcn/bcn.shp")
+amb_shp <- sf::st_read("01.Data/amb.gpkg")
+plot(amb_shp[1])
 
 
+dplyr::glimpse(amb_shp)
 
 
+##################################################
+# --- Here we plot entire pregnancy LUR NO2 --- # 
+##################################################
 
+ggplot() + 
+geom_sf(data = amb_shp, fill = "grey80")+ 
+theme(plot.title = element_text(color = "black", size = 14, face = "bold")) +
+facet_grid(. ~ "NO2 LUR model") + theme_bw()
 
-
-
-
-
-
+# Calculate average NO2 using aggregate
+average_no2 <- aggregate(no2_lur ~ subject_id, data = lur_estimates_plot, FUN = mean)
 
 
